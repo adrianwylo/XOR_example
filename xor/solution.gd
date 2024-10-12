a487af12a4de7a51ce192a82e6f1bde3423ac9f5
adrianwylo <62461216+adrianwylo@users.noreply.github.com>
2024-10-11 23:07:57 -0700

    created vertex and edge class for metadata storage

--- xor/solution.gd
+++ xor/solution.gd
@@ -4,7 +4,61 @@ extends Node2D
 #scale of 1 to 5
 var diff_max = 5
 
-#variables for solution creation---------------------------------------------------
+#Define an edge class
+class Edge:
+	var pos_start: Vector2
+	var pos_end: Vector2
+	var len: int
+	var open: bool
+
+	# Constructor
+	func _init(x1: int, x2: int, y1: int, y2: int, startindex: int, isopen: bool):
+		pos_start = Vector2(x1, y1)
+		pos_end = Vector2(x2, y2)
+		len = pos_start.distance_to(pos_end)
+		open = isopen
+
+	func close():
+		open = false
+
+	func display_info():
+		print("Start Position: ", pos_start)
+		print("End Position: ", pos_end)
+		print("Length: ", len)
+		print("Open: ", open)
+
+#Define a vertex class
+class Vertex:
+	var nextVertex: Vertex
+	var vertex_pos: Vector2
+	
+	#you know which is the next vertex...
+	func _init(pos: Vector2, next: Vertex = null):
+		nextVertex = next
+		vertex_pos = pos
+	
+	 # Points the current vertex to a new vertex
+	func addVertex(new: Vertex):
+		new.nextVertex = nextVertex 
+		nextVertex = new
+
+	# Removes the next vertex by skipping over it
+	func subVertex():
+		if nextVertex != null:
+			nextVertex = nextVertex.nextVertex
+
+	 # Function to return ordered polygon vertices starting at first
+	func make_polygon_array() -> Array:
+		var polygon = []
+		var current_vertex = self 
+		while current_vertex != null:
+			polygon.append(current_vertex.coordinate)
+			current_vertex = current_vertex.nextVertex
+		assert(polygon.size() > 2, "not a shape!")
+		assert(polygon[0] == polygon[polygon.size() - 1], "First and last vertex are diff!")
+		return polygon
+
+#variables for solution creation------------------------------------------------
 #must be creater than 0
 var max_shape_count # for now directly equal to difficulty * 2
 #ultimate # of shapes
@@ -22,17 +76,23 @@ var total_area
 #array of areas of all shape entities
 var shape_areas
 
-#
+#array of array of vertices that make up polygon
+var mapped_shapes
 #WILL BE MORE... (consolidate difficulty rating within this function)
 #-------------------------------------------------------------------------------
 
+#randomly chooses a direction (input = blacklisted options)
+func choose_direction(no: Array) -> int:
+	assert(len(no) < 4, "can't say no to all 4 buddy")
+	var dir = randi_range(0,3)
+	if dir in no:
+		return choose_direction(no)
+	else:
+		return dir
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
@@ -65,7 +125,6 @@ func make_area_bins(diff, total_area, shape_count) -> Array:
 				continue
 	return shape_areas
 
-
 #populates parameters for solution generator
 func process_difficulty(diff) -> void:
 	print("difficulty is " + str(diff))
@@ -92,18 +151,30 @@ func process_difficulty(diff) -> void:
 func probability_check(prob: float) -> bool:
 	return randf() <= prob
 
-#randomly chooses a direction (input = blacklisted options)
-func choose_direction(no: Array) -> int:
-	assert(len(no) < 4, "can't say no to all 4 buddy")
-	var dir = randi_range(0,3)
-	if dir in no:
-		return choose_direction(no)
-	else:
-		return dir
-	
-	
+#the following functions return vertex linked list:
+#return rect (todo)
+func plot_rect(start: Vector2, end: Vector2, x: int, y: int, direction: int, is_baseshape: bool = false) -> Array:
+	if is_baseshape:
+		
+	assert(direction < 4 and direction >= 0, "thats not a direction!")
+	match direction:
+		0: #up
+			
+		1: #down
+			
+		2: #left
+			
+		3: #right
+			
+		
+		# Default case (similar to 'else')
+	var tl = tl_ref
+	var tr = tl_ref + Vector2(x,0)
+	var bl = tl_ref + Vector2(0,y)
+	var br = tl_ref + Vector2(x,y)
+	return [tl, tr, bl, br]
 
-
+#Populates mapped_shapes (Todo)
 func map_shapes() -> void:
 	#NOTES:
 	#[0 = up, 1 = down, 2 = left, 3 = right]
@@ -113,10 +184,13 @@ func map_shapes() -> void:
 	#    /  |                                  | /
 	#   /___|                                  |/
 	#
-	#for 22.5 degree angles, we use probability check to determine which side is longer
+	#for 22.5 degree angles, we use probability check to determine which side is
+	#longer 
 	
 	#for 2 choice items (l/r and u/d)
 	#[0 = u/d, 1 = l/r]
+	
+	#SHAPES WILL BE DRAWN WITH CLOCKWISE DIRECTION
 	
 	for shape_areas in shape_areas:
 		#this it the top/leftmost corner of shape (does not have to be vertex)
@@ -137,8 +211,13 @@ func map_shapes() -> void:
 		var unfilled_area = shape_areas
 		
 		
-		#create base shape
 		
+		#create base shape
+		#starting objects = trianngle types x1, x2
+		# (arbitrary)     = rect of 1 x 1, 2 x 1, 2 x 3
+		if probability_check(angle_225_prob):
+			var shape_allignment = choose_direction([])
+			
 		#choose shape
 		#choose rotation
 		
@@ -149,11 +228,8 @@ func map_shapes() -> void:
 			#choose shape
 			#calc edge possibilities
 			#update reference_pos
-			
-			
-		
-		
 
+#script call
 func _on_main_init_solution(node_count: Variant, difficulty: Variant) -> void:
 	#note that max_shape_count is moreso tied to node count than anything
 	max_shape_count = node_count*node_count/3
@@ -161,6 +237,7 @@ func _on_main_init_solution(node_count: Variant, difficulty: Variant) -> void:
 	total_area = floor(node_count*node_count*0.9)
 	
 	process_difficulty(difficulty)
+	#populates
 	map_shapes()
 	
 	
