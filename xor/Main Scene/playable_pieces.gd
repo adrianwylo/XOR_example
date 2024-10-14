53f8ef12189259e9b6e53f9d74b6bb4719015825
adrianwylo <62461216+adrianwylo@users.noreply.github.com>
2024-10-13 22:14:29 -0700

    created timer lock to ensure one drag at a time

--- xor/Main Scene/playable_pieces.gd
+++ xor/Main Scene/playable_pieces.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 @export var new_shape: PackedScene
 
+#reference to child node for timing lock
+@onready var lock_timer = $Timer
+
 #Track if the object is being dragged
 var dragging = false 
 
@@ -44,6 +47,7 @@ func _on_piece_occupy_drag(id) -> void:
 
 #ack function for letting go of piece (requests grid nodes for info)
 func _on_piece_free_drag(id, corner_pos) -> void:
+	print(dragged_shape_id)
 	if dragged_shape_id == id:
 		emit_signal("snap", id, corner_pos)
 
@@ -51,11 +55,17 @@ func _on_piece_free_drag(id, corner_pos) -> void:
 func _on_grid_pieces_snap_info(grid_pos: Variant) -> void:
 	emit_signal("start_snap", grid_pos, dragged_shape_id)
 
-#reopen function for next piece picked
+# Reopen function for next piece picked
 func _on_piece_continue_q(id) -> void:
 	if dragged_shape_id == id:
-		dragged_shape_id = -1
-		dragging = false
+		#gives time for snap so piece can't be taken
+		lock_timer.start(0.5)
+
+#finishes snap operation after timer and reopens drag
+func _on_lock_timer_timeout() -> void:
+	dragged_shape_id = -1
+	dragging = false 
+	lock_timer.stop()
 
 #creates pieces on board from list of list of positions
 func _on_solution_create_pieces(shape_pieces: Variant, pos_dic: Variant) -> void:
